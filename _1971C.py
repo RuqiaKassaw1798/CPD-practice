cb1f20175bd536b8b8597b00408da6130ef07154
Ruqia Kassaw Mengistu <ruqiakassaw48@gmail.com>
2026-05-10 07:27:50 +0300

    Implement logic to check range conditions

---
+++ _1971C.py
@@ -0,0 +1,11 @@
+t = int(input())
+for i in range(t):
+    a, b, c, d = map(int, input().split())
+    if a > b:
+        a, b = b, a
+    x = a < c < b
+    y = a < d < b
+    if x != y:
+        print("YES")
+    else:
+        print("NO")
